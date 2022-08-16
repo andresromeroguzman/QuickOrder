348690f81ca9dc504fea78738f9028f9f8768eb0
andresromeroguzman <75345836+andresromeroguzman@users.noreply.github.com>
2022-08-15 23:30:17 -0500

    ajustes

--- about.php
+++ about.php
@@ -1,33 +1,20 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>About</title>
-
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Custom CSS -->
     <link href="css/business-casual.css" rel="stylesheet">
-
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 	<?php
 		$Username = null;
 		if(!empty($_SESSION["Username"]))
@@ -36,9 +23,7 @@
 		}
 	?>
 </head>
-
 <body">
-
     <div class="container-fluid">
     <nav class="navbar navbar-default" role="navigation">
         <div class="container-fluid bg-dark text-dark ">
@@ -74,12 +59,9 @@
             <h4>Realiza tu pedido y reclama tu orden en tiempo exacto</h4>
             <img src="img/Quienes.png" alt="Quienes somos" class="QuienesImg">
             </div>
+        </div>        
         </div>
-        
-        </div>
-    </div>
-    <!-- /.container -->
-
+    </div>    
     <footer class="container-fluid">                 
                 <div class="footer col-lg-12 text-center">
                     <p>
@@ -95,24 +77,18 @@
             </div>
         </div>
         
-    </footer>
-    
+    </footer>    
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-	<script>
-		
+	<script>		
 		function ManagementOnclick(){
 			if(confirm("Solo los administradores tienen permitido acceder a esta página. Inicie sesión como administrador.") == true)
 			{
 				window.open("Login.php?Role=Admin","_self",null,true);
 			}
-		}
-		
+		}		
     </script>
-
 </body>
-
 </html>
