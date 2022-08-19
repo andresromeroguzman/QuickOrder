7e00f61595ab78876c678e6334af7f5187beb1ad
gabetanc <110784592+gabetanc@users.noreply.github.com>
2022-08-18 23:19:16 -0400

    Update ProductosLista.php

--- ProductosLista.php
+++ ProductosLista.php
@@ -10,6 +10,7 @@
     <title>Lista de productos</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/business-casual.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
 	<?php
@@ -80,8 +81,8 @@
 									<td><?php echo $Rows[6]; ?></td>
 									<td>
 										<!-- Botones de accion -->
-									<a href="#" onclick="ProductOnlick('Edit',<?php echo $Rows[0]; ?>)">Editar</a> | 
-									<a href="#" onclick="ProductOnlick('Delete',<?php echo $Rows[0]; ?>)">Eliminar</a>
+									<a href="#" onclick="ProductOnlick('Edit',<?php echo $Rows[0]; ?>)"><span style="color: green;"><abbr title ="Editar"><i class="fa-solid fa-pencil fa-xl"></i></abbr></span></a> |
+									<a href="#" onclick="ProductOnlick('Delete',<?php echo $Rows[0]; ?>)"><span style="color: Tomato;"><abbr title ="Eliminar"><i class="fa-solid fa-trash-can fa-xl"></i></abbr></span></a>
 									</td>
 									<?php endwhile; ?>
 								</tr>
